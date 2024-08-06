04c8c7c5ea07353a9a6bbd996d18c9e93bc9333a
Brandon Supinski <66806979+bsupinski@users.noreply.github.com>
2024-08-06 14:32:38 -0400

    added readme

---
+++ student-files/README.md
@@ -0,0 +1,9 @@
+Team treehouse project 5 for its full stack tech degree
+
+Goal of the project is to use fetch api to add randomuser api info into cards presenting their information.
+
+Selecing a card should bring up a modal with a new card of the selected card that has more information on that pereson.
+Next/prev buttons should cycle through the cards representing the order they showed on load.
+
+CSS Changes
+Added box shadow and scale on hover to the card
--- student-files/js/script.js
+++ student-files/js/script.js
@@ -7,69 +7,6 @@ const userAPI = `https://randomuser.me/api/?results=12`;
 
 const usersState = [];
 
-function modalHTML() {
-  const modalContainer = document.createElement("div");
-  modalContainer.classList.add("modal-container");
-
-  const modal = document.createElement("div");
-  modal.classList.add("modal");
-
-  const closeButton = document.createElement("button");
-  closeButton.classList.add("modal-close-btn");
-  closeButton.setAttribute("type", "button");
-  closeButton.setAttribute("id", "modal-close-btn");
-
-  const strongText = document.createElement("strong");
-  strongText.innerText = "X";
-
-  const modalInfoContainer = document.createElement("div");
-  modalInfoContainer.classList.add("modal-info-container");
-
-  const modalImg = document.createElement("img");
-  modalImg.classList.add("modal-img");
-  modalImg.setAttribute("src", `https://placehold.it/125x125`);
-  modalImg.setAttribute("alt", `temp`);
-
-  const modalName = document.createElement("div");
-  modalName.classList.add("modal-name", "cap");
-  modalName.setAttribute("id", "name");
-  modalName.innerText = `Temp`;
-
-  const modalEmail = document.createElement("div");
-  modalEmail.classList.add("modal-text");
-  modalEmail.innerText = `Temp`;
-
-  const modalCity = document.createElement("div");
-  modalCity.classList.add("modal-text", "cap");
-  modalCity.innerText = `Temp`;
-
-  const modalPhone = document.createElement("div");
-  modalPhone.classList.add("modal-text");
-  modalPhone.innerText = `Temp`;
-
-  const modalAddress = document.createElement("div");
-  modalAddress.classList.add("modal-text");
-  modalAddress.innerText = `Temp`;
-
-  const modalBirthday = document.createElement("div");
-  modalBirthday.classList.add("modal-text");
-  modalBirthday.innerText = `Temp`;
-
-  closeButton.append(strongText);
-  modalInfoContainer.append(
-    modalImg,
-    modalName,
-    modalEmail,
-    modalCity,
-    modalAddress,
-    modalBirthday
-  );
-
-  modal.append(closeButton, modalInfoContainer);
-  modalContainer.append(modal);
-  body.append(modalContainer);
-}
-
 // Create HTML
 function cardHTML(person) {
   const cardDiv = document.createElement("div");
@@ -108,8 +45,148 @@ function cardHTML(person) {
   gallery.append(cardDiv);
 }
 
-function addHTML(html, parent) {
-  parent.innerHTML = html;
+function modalHTML(person) {
+  const modalContainer = document.createElement("div");
+  modalContainer.classList.add("modal-container");
+
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+
+  const closeButton = document.createElement("button");
+  closeButton.classList.add("modal-close-btn");
+  closeButton.setAttribute("type", "button");
+  closeButton.setAttribute("id", "modal-close-btn");
+
+  const strongText = document.createElement("strong");
+  strongText.innerText = "X";
+
+  const modalInfoContainer = document.createElement("div");
+  modalInfoContainer.classList.add("modal-info-container");
+
+  const modalImg = document.createElement("img");
+  modalImg.classList.add("modal-img");
+  modalImg.setAttribute("src", `${imgSizeCheck(person.picture)}`);
+  modalImg.setAttribute("alt", `${person.name.first} ${person.name.last}`);
+
+  const modalName = document.createElement("div");
+  modalName.classList.add("modal-name", "cap");
+  modalName.setAttribute("id", "name");
+  modalName.innerText = `${person.name.first} ${person.name.last}`;
+
+  const modalEmail = document.createElement("div");
+  modalEmail.classList.add("modal-text", "modal-email");
+  modalEmail.innerText = `${person.email}`;
+
+  const modalCity = document.createElement("div");
+  modalCity.classList.add("modal-text", "modal-city", "cap");
+  modalCity.innerText = `${person.location.city}`;
+
+  const modalPhone = document.createElement("div");
+  modalPhone.classList.add("modal-text", "modal-phone");
+  modalPhone.innerText = `${person.phone}`;
+
+  const modalAddress = document.createElement("div");
+  modalAddress.classList.add("modal-text", "modal-address");
+  modalAddress.innerText = `${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state}`;
+
+  const modalBirthday = document.createElement("div");
+  modalBirthday.classList.add("modal-text", "modal-birthday");
+  modalBirthday.innerText = `${formatBday(person.dob.date)}`;
+
+  const btnContainer = document.createElement("div");
+  btnContainer.setAttribute("type", "button");
+  btnContainer.classList.add("modal-btn-container");
+
+  const prevBtn = document.createElement("button");
+  prevBtn.setAttribute("type", "button");
+  prevBtn.setAttribute("id", "modal-prev");
+  prevBtn.classList.add("modal-prev", "btn");
+  prevBtn.innerText = "Prev";
+
+  const nextBtn = document.createElement("button");
+  nextBtn.setAttribute("type", "button");
+  nextBtn.setAttribute("id", "modal-next");
+  nextBtn.classList.add("modal-next", "btn");
+  nextBtn.innerText = "Next";
+
+  btnContainer.append(prevBtn, nextBtn);
+
+  closeButton.append(strongText);
+  modalInfoContainer.append(
+    modalImg,
+    modalName,
+    modalEmail,
+    modalPhone,
+    modalCity,
+    modalAddress,
+    modalBirthday
+  );
+
+  modal.append(closeButton, modalInfoContainer, btnContainer);
+  modalContainer.append(modal);
+  body.append(modalContainer);
+}
+
+function modalCardHTML(person) {
+  const personImg = document.querySelector(".modal-img");
+  const personName = document.querySelector(".modal-name");
+  const personEmail = document.querySelector(".modal-email");
+  const personCity = document.querySelector(".modal-city");
+  const personPhone = document.querySelector(".modal-phone");
+  const personAddress = document.querySelector(".modal-address");
+  const personBirthday = document.querySelector(".modal-birthday");
+
+  personImg.src = `${imgSizeCheck(person.picture)}`;
+  personImg.alt = `${person.name.first} ${person.name.last}`;
+  personName.innerText = `${person.name.first} ${person.name.last}`;
+  personEmail.innerText = `${person.email}`;
+  personCity.innerText = `${person.location.city}`;
+  personPhone.innerText = `${person.phone}`;
+  personAddress.innerText = `${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state}`;
+  personBirthday.innerText = `${formatBday(person.dob.date)}`;
+}
+
+// Helpers
+function formatBday(timeStamp) {
+  const justTheDate = timeStamp.split("T")[0].split("-");
+  const bday = `${justTheDate[1].padStart(2, "0")}/${justTheDate[2].padStart(
+    2,
+    "0"
+  )}/${justTheDate[0].padStart(2, "0")}`;
+  return bday;
+}
+
+function imgSizeCheck(img) {
+  return `${img.large} ` ? `${img.large} ` : `${img.small}`;
+}
+
+// Button functions
+function nextBtn(currentPerson) {
+  const nextBtn = document.querySelector(".modal-next");
+  nextBtn.addEventListener("click", () => {
+    const currentIndex = usersState.indexOf(currentPerson);
+    if (currentIndex >= usersState.length - 1) {
+      currentPerson = usersState[0];
+      modalCardHTML(currentPerson);
+    } else {
+      currentPerson = usersState[currentIndex + 1];
+      modalCardHTML(currentPerson);
+    }
+  });
+}
+
+function prevBtn(currentPerson) {
+  const prevBtn = document.querySelector(".modal-prev");
+  prevBtn.addEventListener("click", () => {
+    const currentIndex = usersState.indexOf(currentPerson);
+    if (currentIndex === 0) {
+      currentPerson = usersState[usersState.length - 1];
+      modalCardHTML(currentPerson);
+    } else {
+      currentPerson = usersState[currentIndex - 1];
+      modalCardHTML(currentPerson);
+    }
+  });
 }
 
 // Set up API call
@@ -128,16 +205,37 @@ async function getUsers(api) {
 async function createUserCards() {
   await getUsers(userAPI);
   gallery.innerHTML = "";
-  console.log(usersState[0]);
   usersState.map((person) => {
     cardHTML(person);
   });
 }
 
 //Event Listeners
+function closeButton() {
+  const close = document.getElementById("modal-close-btn");
+  const modal = document.querySelector(".modal-container");
+  close.addEventListener("click", (e) => {
+    modal.remove();
+  });
+}
+
 gallery.addEventListener("click", (e) => {
   if (!e.target.classList.contains("gallery")) {
-    modalHTML();
+    let currentPerson = e.target
+      .closest(".card")
+      .querySelector(".card-info-container")
+      .querySelector(".card-name").innerText;
+    usersState.forEach((bday) => {});
+    usersState.forEach((person) => {
+      if (`${person.name.first} ${person.name.last}` === currentPerson) {
+        currentPerson = person;
+      }
+    });
+
+    modalHTML(currentPerson);
+    closeButton();
+    nextBtn(currentPerson);
+    prevBtn(currentPerson);
   }
 });
 
@@ -150,4 +248,6 @@ search.addEventListener("input", (e) => {
     else person.parentNode.parentNode.style.display = "flex";
   });
 });
+
+// Load cards on page load
 createUserCards();
